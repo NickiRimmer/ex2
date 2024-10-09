28bf5266051b92f75150bf7b9ad6284d73c169f8
NickiRimmer <nicki_rimmer@inbox.ru>
2024-10-09 23:29:31 +0300

    Задание 5

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-function calс() {
+function calc(event) {
+    event.preventDefault();
     let res=document.getElementById('res');
-    //alert('yeah');
     let a=document.getElementsByName('kolichestvo');
     let b=document.getElementsByName('tovar');
     let ai = Number(a[0].value);
     console.log(ai * b[0].value);
-    console.log(a[0].value);
     if (/^\d+$/.test(a[0].value)){
         res.innerHTML=ai * b[0].value;
     }
@@ -13,4 +12,11 @@ function calс() {
         res.innerHTML = 'Uncorrect input';
         alert('Неверный ввод!');
     }
-}
+    return false;
+};
+
+window.addEventListener('DOMContentLoaded', function (event) {
+    console.log("DOM fully loaded and parsed");
+    let button = document.getElementById("button1");
+    button.addEventListener("click", calc);
+});
