3864197e3cbb084b7c7446ba9cfdf2e3d0372170
NickiRimmer <160492646+NickiRimmer@users.noreply.github.com>
2024-10-10 11:32:18 +0300

    Update script.js to delete warnings

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function calc(event) {
     event.preventDefault();
-    let res=document.getElementById('res');
-    let a=document.getElementsByName('kolichestvo');
-    let b=document.getElementsByName('tovar');
+    let res=document.getElementById("res");
+    let a=document.getElementsByName("kolichestvo");
+    let b=document.getElementsByName("tovar");
     let ai = Number(a[0].value);
     console.log(ai * b[0].value);
     if (/^\d+$/.test(a[0].value)){
         res.innerHTML=ai * b[0].value;
     }
     else{
-        res.innerHTML = 'Uncorrect input';
-        alert('Неверный ввод!');
+        res.innerHTML = "Uncorrect input";
+        alert("Неверный ввод!");
     }
     return false;
-};
+}
 
-window.addEventListener('DOMContentLoaded', function (event) {
+window.addEventListener("DOMContentLoaded", function (event) {
     console.log("DOM fully loaded and parsed");
     let button = document.getElementById("button1");
     button.addEventListener("click", calc);
