15c1cf161046b1e16841f9cf1025621b5a7788fb
NickiRimmer <nicki_rimmer@inbox.ru>
2024-10-18 20:28:20 +0300

    Исправление ошибок

--- script2.js
+++ script2.js
@@ -14,15 +14,15 @@ function recalc(event) {
         if (radio.checked) {
             radioVal = radio.value;
         }
-    })
+    });
 
     let cost = 0;
-    if (radioVal == "1") {
+    if (radioVal === "1") {
         opt1.style.display = "none";
         check.style.display = "none";
         cost = 100;
     }
-    else if (radioVal == "2") {
+    else if (radioVal === "2") {
         opt1.style.display = "block";
         check.style.display = "none";
         cost = 200 + parseInt(opt1.value);
@@ -57,17 +57,13 @@ function recalc(event) {
 
 
 
-window.addEventListener('DOMContentLoaded', function (event) {
+window.addEventListener("DOMContentLoaded", function (event) {
     console.log("DOM fully loaded and parsed // script2");
-    /*let button = document.getElementById("button1");
-    button.addEventListener("click", calc);*/
 
-    
 
     let r = document.querySelectorAll(".radios input[type=radio]");
     r.forEach(function (radio) {
-        
-        radio.addEventListener("change", recalc);    
+        radio.addEventListener("change", recalc);
     });
 
     let s = document.getElementsByName("opt1");
@@ -75,14 +71,13 @@ window.addEventListener('DOMContentLoaded', function (event) {
 
     let c = document.getElementsByName("check");
     c.forEach(function (checkbox) {
-        checkbox.addEventListener("change", recalc)
-    })
+        checkbox.addEventListener("change", recalc);
+    });
 
     let c1 = document.getElementsByName("check1");
     let c2 = document.getElementsByName("check2");
     c1[0].addEventListener("change", recalc);
     c2[0].addEventListener("change", recalc);
-    
 
     let k = document.getElementsByName("kol2");
     k[0].addEventListener("blur", recalc);
